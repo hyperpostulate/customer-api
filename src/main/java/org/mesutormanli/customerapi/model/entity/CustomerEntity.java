0c161ad747efc3db50cad116a78473eb9c30000a
hyperpostulate <mesutormanli@gmail.com>
2024-05-18 19:23:13 +0300

    upgrades java version to 21, spring-boot version to 3.2.5

--- src/main/java/org/mesutormanli/customerapi/model/entity/CustomerEntity.java
+++ src/main/java/org/mesutormanli/customerapi/model/entity/CustomerEntity.java
@@ -1,14 +1,13 @@
 package org.mesutormanli.customerapi.model.entity;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-
-import javax.persistence.*;
 
 @Data
 @Entity
